6efff5219ceca438f3e13f76c77dda460dcd2a34
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2022-01-24 16:14:52 +0330

    Add String method to comment.go

--- model/comment.go
+++ model/comment.go
@@ -1,6 +1,9 @@
 package model
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type Comment struct {
 	id        int64
@@ -9,4 +12,8 @@ type Comment struct {
 	approved  bool
 	createdAt time.Time
 	movieID   int
+}
+
+func (c *Comment) String() string {
+	return fmt.Sprint(c.comment, ":", c.createdAt)
 }
