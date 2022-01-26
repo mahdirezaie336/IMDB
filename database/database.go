72d920ae8bb71b11c254d20ce187230fc8378701
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2022-01-26 22:22:35 +0330

    Add database New function

--- database/database.go
+++ database/database.go
@@ -3,6 +3,17 @@ package database
 import "database/sql"
 
 type Database struct {
-	db      sql.DB
+	db      *sql.DB
 	address string
+}
+
+func New(address string) (Database, error) {
+	db, err := sql.Open("mysql", "root@tcp(172.17.0.2:3306)/imdb")
+	if err != nil {
+		return Database{}, err
+	}
+	return Database{
+		db:      db,
+		address: address,
+	}, nil
 }
